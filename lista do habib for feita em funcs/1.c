8a0a969c9c0200138f482378c4f9e0f27f9753df
darkvictor13 <01victoralmeida@gmail.com>
2019-06-24 16:46:19 -0300

    arrumei

--- lista do habib for feita em funcs/1.c
+++ lista do habib for feita em funcs/1.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
 void print_alfabeto () {
   for (int a = 'a'; a <= 'z'; a++)
-=======
-void print_alfabeto (int a) {
-  for (a = 'a'; a <= 'z'; a++)
->>>>>>> 2542b5b457e381c6a8844106004173243f5949e8
     printf ("%c\n",a);
 }
 
 int main () {
-<<<<<<< HEAD
   print_alfabeto();
-=======
-  int x;
-  print_alfabeto(x);
->>>>>>> 2542b5b457e381c6a8844106004173243f5949e8
   return 0;
 }
--- lista do habib for feita em funcs/2.c
+++ lista do habib for feita em funcs/2.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
 void print_alfabeto () {
   for (int a = 'z'; a >= 'a'; a--)
-=======
-void print_alfabeto (int a) {
-  for (a = 'z'; a >= 'a'; a--)
->>>>>>> 2542b5b457e381c6a8844106004173243f5949e8
     printf ("%c\n",a);
 }
 
 int main () {
-<<<<<<< HEAD
   print_alfabeto();
-=======
-  int x;
-  print_alfabeto(x);
->>>>>>> 2542b5b457e381c6a8844106004173243f5949e8
   return 0;
 }
