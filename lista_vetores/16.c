44ec653919cbc140b4189e5218a22553f3b7b804
darkvictor13 <01victoralmeida@gmail.com>
2019-08-02 09:45:41 -0300

    ashas

---
+++ lista_vetores/16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void preenche_vet (int v1[],int v2[],int n) {
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d", &v1[i],&v2[i]);
+  }
+}
+
+void preenche_vet_dobro (int v3[],int v1[],int v2[],int n) {
+  int i,c;
+  for (i = 0,c = 0; c < n; i+=2,c++) {//easter egg p prox ano
+    v3[i] = v1[c];
+    v3[i+1] = v2[c];
+  }
+}
+
+void print_vet (int v[],int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d\n", v[i]);
+  }
+}
+
+int main () {
+  int tamanho;
+  scanf("%d", &tamanho);
+  int v1[tamanho],v2[tamanho],v3[tamanho];
+
+  preenche_vet(v1,v2,tamanho);
+  preenche_vet_dobro (v3,v1,v2,tamanho);
+  print_vet (v3,(tamanho*2));
+  return 0;
+}
