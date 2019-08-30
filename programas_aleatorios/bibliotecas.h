680798113c69de41d1935266b84f3a4b1bb2d9bf
darkvictor13 <01victoralmeida@gmail.com>
2019-08-30 16:46:12 -0300

    tchau atom

--- programas_aleatorios/bibliotecas.h
+++ programas_aleatorios/bibliotecas.h
@@ -3,6 +3,7 @@
 #include "//home/victor/github/ProgramasEmC1/minhas bibliotecas/vet.h"
 #include "//home/victor/github/ProgramasEmC1/minhas bibliotecas/stringV.h"
 #include <math.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
--- strings/bibliotecas.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "//home/victor/github/ProgramasEmC1/minhas bibliotecas/vet.h"
-#include "//home/victor/github/ProgramasEmC1/minhas bibliotecas/stringV.h"
-#include "//home/victor/github/ProgramasEmC1/minhas bibliotecas/numerico.h"
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-#include <locale.h>
