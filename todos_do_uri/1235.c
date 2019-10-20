1ef88c7505d6616adecf9463b1b7ff2ace852df5
darkvictor13 <01victoralmeida@gmail.com>
2019-10-20 16:05:08 -0300

    uri++;

--- todos_do_uri/1235.c
+++ todos_do_uri/1235.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 void decifra(char *dest,char *org) {
-  for (; *org; org++) {
-    
+  int t = strlen(org);
+  char *metade = (org + (t/2)),*ultimo_caracter = (org + t - 1);
+
+  for (metade--; metade >= org; metade--,dest++) {
+    *dest = *metade;
   }
+
+  metade = (org + (t/2));
+
+  for (; metade <= ultimo_caracter; ultimo_caracter--,dest++) {
+    *dest = *ultimo_caracter;
+  }
+  *dest = '\0';
 }
 
 int main () {
+  char str1[101],str2[101];
+  int i;
+
+  for (scanf("%d%*c",&i); i > 0; i--) {
+    scanf("%[^\n]%*c",str1);
+    decifra(str2,str1);
+    printf("%s\n",str2);
+  }
   return 0;
 }
--- todos_do_uri/2062.c
+++ todos_do_uri/2062.c
@@ -2,5 +2,28 @@
 #include <string.h>
 
 int main () {
+  int qnt_str,tam_str;
+  char str[22];
+
+  for (scanf("%d",&qnt_str); qnt_str > 0; qnt_str--) {
+    scanf("%s",str);
+    tam_str = strlen(str);
+
+    if (tam_str == 3) {
+      if (str[0] == 'U' && str[1] == 'R') {
+        printf("URI");
+      }else if (str[0] == 'O' && str[1] == 'B') {
+        printf("OBI");
+      }else {
+        printf("%s",str);
+      }
+    }else {
+      printf("%s",str);
+    }
+    if (qnt_str > 1) {
+      printf(" ");
+    }
+  }
+  printf("\n");
   return 0;
 }
