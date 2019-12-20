eac41f6263dc7f4638c84c1d42918b6979303b2e
darkvictor13 <01victoralmeida@gmail.com>
2019-12-20 11:26:33 -0300

    aaaaa

--- lista_habib_for/4.c
+++ lista_habib_for/4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main () {
-   int j,c,i;
-   c = 'z';
-   i = 0;
-   scanf("%d",&j);
-   while (i<j){
-     printf("%c\n",c);
-     i++;
-     c--;
-   }
-   printf("o programa ficou em loop %d vezes\n",i);
+  int j,c,i;
+  c = 'z';
+  i = 0;
+  scanf("%d", &j);
+  while (i < j){
+    printf("%c\n",c);
+    i++;
+    c--;
+  }
+  printf("o programa ficou em loop %d vezes\n",i);
 }
